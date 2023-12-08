6faa70ea9595493cd59d8966900a8cdfe61b3799
kahkkk <emmadarkbunny0@gmail.com>
2023-12-08 16:21:20 -0300

    feat: read a file and print the first N lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+use std::env;
+
+// cargo run -- <FILE> [lines] <TOTAL = 10>
 fn main() {
-    println!("Olá mundo!");
+  let args: Vec<String> = env::args().collect();
+  let file_path = &args[1];
+  let total_lines: i32;
+
+  if args.len() == 3 {
+    total_lines = args[2].parse().unwrap();
+  } else {
+    total_lines = 10;
+  }
+
+  let contents = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file");
+  let lines: Vec<&str> = contents.split("\n").collect();
+
+  let mut idx: i32 = 0;
+  let mut text: String = "".to_owned();
+
+  while idx < total_lines {
+    text.push_str("\n");
+    text.push_str(&lines[idx as usize]);
+    idx += 1;
+  }
+  
+  print!("{}", text);
 }
