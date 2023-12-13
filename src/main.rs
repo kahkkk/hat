66f82597d7ca0f4ce3c3a6b1b9874cec828593d8
sads4ndwich <caiocamargo007@gmail.com>
2023-12-13 15:05:28 -0300

    refactor: create function to read and store args into hashmap and use it to get args

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,54 @@
 use std::fs;
 use std::env;
+use std::collections::HashMap;
 
-// cargo run -- <FILE> [lines] <TOTAL = 10>
-fn main() {
+fn read_args() -> HashMap<String, String> {
   let args: Vec<String> = env::args().collect();
-  let file_path = &args[1];
-  let total_lines: i32;
 
-  if args.len() == 3 {
-    total_lines = args[2].parse().unwrap();
-  } else {
-    total_lines = 10;
+  let mut idx: usize = 1;
+  let mut params = HashMap::new();
+  
+  while idx < args.len() {
+    let curr = &args[idx];
+
+    if curr.starts_with("--") {
+      let has_next = idx + 1 < args.len();
+      let is_boolean = !has_next || args[idx + 1].starts_with("--");
+
+      if has_next && !is_boolean {
+        params.insert(curr.to_owned(), args[idx + 1].to_owned());
+        idx += 2;
+      } else if is_boolean {
+        params.insert(curr.to_owned(), String::from("true").to_owned());
+        idx += 1;
+      }
+    } else {
+      idx += 1;
+    }
   }
+
+  params
+}
+
+fn main() {
+  let params = read_args();
+  
+  let file_path = params
+    .get("--file")
+    .expect("File path is required");
+
+  let total_lines: i32 = params
+    .get("--lines")
+    .and_then(|n| Some(n.parse::<i32>().unwrap()))
+    .unwrap_or(10);
 
   let contents = fs::read_to_string(file_path)
     .expect("Should have been able to read the file");
-  let lines: Vec<&str> = contents.split("\n").collect();
 
-  let mut idx: i32 = 0;
-  let mut text: String = "".to_owned();
+  let lines = &contents
+    .split("\n")
+    .collect::<Vec<&str>>()[..total_lines as usize]
+    .join("\n");
 
-  while idx < total_lines {
-    text.push_str("\n");
-    text.push_str(&lines[idx as usize]);
-    idx += 1;
-  }
-  
-  print!("{}", text);
+  print!("{}", lines)
 }
