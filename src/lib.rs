9c0b3a6e01a97fd9787abcb908248c3643f48277
kahkkk <emmadarkbunny0@gmail.com>
2023-12-15 13:08:15 -0300

    refactor: improve read first N lines using `.take()` instead of `.take_while()`

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,9 @@ impl Hat {
     let contents = fs::read_to_string(&config.path)
       .expect("File does not exists");
 
-    let mut i: u32 = 0;
     let lines = contents
       .lines()
-      .take_while(|_| { let tmp = i; i += 1; tmp < config.total_lines })
+      .take(config.total_lines as usize)
       .collect::<Vec<_>>()
       .join("\n");
 
