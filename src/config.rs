67dbd9756c666196ac322f6884a94fe0157092ac
kahkkk <emmadarkbunny0@gmail.com>
2023-12-15 11:32:32 -0300

    refactor: create own config module

---
+++ src/config.rs
@@ -0,0 +1,21 @@
+use crate::{ Parser };
+
+pub struct Config {
+  pub path: String,
+  pub total_lines: u32
+}
+
+impl Config {
+  pub fn build(args: Parser) -> Config {
+    let path = args
+      .get("--file")
+      .expect("File path must be required.");
+
+    let total_lines = args
+      .get("--lines")
+      .and_then(|n| Some(n.parse::<u32>().unwrap()))
+      .unwrap_or(10);
+
+    Config { path: String::from(path), total_lines }
+  }
+}
